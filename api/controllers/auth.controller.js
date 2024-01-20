f9beb3e1a2d15b3dfab3449d394669f138743afa
VikumChathuranga22434 <127230237+VikumChathuranga22434@users.noreply.github.com>
2024-01-20 23:16:25 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
 
@@ -25,4 +27,37 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 
+};
+
+export const signin = async (req, res, next) => {
+    
+    // getting the email and the password from the signin form
+    const { email, password } = req.body;
+
+    try {
+
+        // checking is the credintails valid
+        const validUser = await User.findOne({ email });
+        // checking the User is valid
+        if (!validUser) return next(errorHandler(404, 'User not Found!'));
+        // checking the password of the user from the database
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        // if is not
+        if (!validPassword) return next(errorHandler(404, "Wrong Credentials"));
+
+        // creating a jwt token for user login
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        // removing the passwod before sending the details to the browser
+        const { password: pass, ...rest } = validUser._doc;
+
+        // saving the token as a cookie
+        res
+          .cookie('access_token', token, { httpOnly: true })
+          .status(200)
+          .json(rest);
+        
+    } catch (error) {
+        next(error);
+    }
 };
