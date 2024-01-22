53f524981601e39f4bafb6c8dfeaa50d91dbe701
VikumChathuranga22434 <127230237+VikumChathuranga22434@users.noreply.github.com>
2024-01-22 21:01:24 +0530

    Add google oAuth functionality (Up to client side)

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+
+export default function OAuth() {
+
+  const handleGoogleClick = async () => {
+    try {
+
+      // initialize the dispatcher
+      const dispatch = useDispatch();
+      
+      // creating a const to get authentication provider
+      const provider = new GoogleAuthProvider();
+
+      // getAuth
+      const auth = getAuth(app);
+
+      // getting the sign up popup 
+      const result = await signInWithPopup(auth, provider);
+
+      // sending the nessassery data to the backend
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        })
+      })
+
+      // convert the data to JSON
+      const data = await res.JSON();
+
+      // use the dispatch sign in success
+      dispatch(signInSuccess(data));
+
+    } catch (error) {
+      console.log('could not sign in with google', error);
+    }
+  };
+
+  return (
+    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with google</button>
+  )
+}
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
 
@@ -74,6 +75,8 @@ export default function SignUp() {
         <button disabled={ loading } className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-85 disabled: opacity-95'>
           { loading? 'Loading...' : 'Sign Up' }
         </button>
+
+        <OAuth />
 
       </form>
 
