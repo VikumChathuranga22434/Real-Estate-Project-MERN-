8ed849bb680d7f52ceef2780b2b2ae3e59e3392b
VikumChathuranga22434 <127230237+VikumChathuranga22434@users.noreply.github.com>
2024-02-01 23:21:15 +0530

    Add create listing route 2

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,10 @@
+import Listing from "../models/listing.model.js";
+
 export const createListing = async (req, res, next) => {
   try {
+    // create a listing
+    const listing = await Listing.create(res.body);
+    return res.status(201).json(listing);
   } catch (error) {
     next(error);
   }
