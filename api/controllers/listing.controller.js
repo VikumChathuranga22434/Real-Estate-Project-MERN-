d4ccdbd5bc7724a4c7b14051ed423b3b95cdfa76
VikumChathuranga22434 <127230237+VikumChathuranga22434@users.noreply.github.com>
2024-02-01 23:48:35 +0530

    Add create listind API route 3

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -3,7 +3,7 @@ import Listing from "../models/listing.model.js";
 export const createListing = async (req, res, next) => {
   try {
     // create a listing
-    const listing = await Listing.create(res.body);
+    const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
     next(error);
