e45ac38983cfa2be683cb57597aba1df2eec435e
VikumChathuranga22434 <127230237+VikumChathuranga22434@users.noreply.github.com>
2024-02-11 01:55:48 +0530

    Add image slider to the listing page 3

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
@@ -62,7 +62,7 @@ export default function Listing() {
                 <div
                   className="h-[550px]"
                   style={{
-                    background: `url(&{url}) center no-repeat`,
+                    background: `url(${url}) center no-repeat`,
                     backgroundSize: "cover",
                   }}
                 ></div>
@@ -74,68 +74,3 @@ export default function Listing() {
     </main>
   );
 }
-
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import SwiperCore from "swiper";
-// import { Navigation } from "swiper/modules";
-// import "swiper/css/bundle";
-
-// // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
-
-// export default function Listing() {
-//   SwiperCore.use([Navigation]);
-//   const [listing, setListing] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(false);
-//   const params = useParams();
-
-//   useEffect(() => {
-//     const fetchListing = async () => {
-//       try {
-//         setLoading(true);
-//         const res = await fetch(`/api/listing/get/${params.listingId}`);
-//         const data = await res.json();
-//         if (data.success === false) {
-//           setError(true);
-//           setLoading(false);
-//           return;
-//         }
-//         setListing(data);
-//         setLoading(false);
-//         setError(false);
-//       } catch (error) {
-//         setError(true);
-//         setLoading(false);
-//       }
-//     };
-//     fetchListing();
-//   }, [params.listingId]);
-
-//   return (
-//     <main>
-//       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
-//       {error && (
-//         <p className="text-center my-7 text-2xl">Something went wrong!</p>
-//       )}
-//       {listing && !loading && !error && (
-//         <div>
-//           <Swiper navigation>
-//             {listing.imageUrls.map((url) => (
-//               <SwiperSlide key={url}>
-//                 <div
-//                   className="h-[550px]"
-//                   style={{
-//                     background: `url(${url}) center no-repeat`,
-//                     backgroundSize: "cover",
-//                   }}
-//                 ></div>
-//               </SwiperSlide>
-//             ))}
-//           </Swiper>
-//         </div>
-//       )}
-//     </main>
-//   );
-// }
