33696ade908157dbf9692afa7e153f08eb705295
VikumChathuranga22434 <127230237+VikumChathuranga22434@users.noreply.github.com>
2024-02-12 14:29:46 +0530

    Add image slider to the listing page 5

--- client/src/pages/ListingTest.jsx
+++ client/src/pages/ListingTest.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+// import { Carousel } from "@material-tailwind/react";
 
 export default function ListingTest() {
   // get the listingId
@@ -48,6 +49,23 @@ export default function ListingTest() {
         <p className="text-center my-7 text-2xl">Something went Wrong...</p>
       )}
       {listing && <p>{listing.name}</p>}
+      {/* <Carousel transition={{ duration: 2 }} className="rounded-xl">
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="image 2"
+          className="h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          alt="image 3"
+          className="h-full w-full object-cover"
+        />
+      </Carousel> */}
     </main>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,3 +6,14 @@ export default {
   },
   plugins: [],
 };
+
+// /** @type {import('tailwindcss').Config} */
+import withMT from "@material-tailwind/react/utils/withMT";
+
+// export default withMT({
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// });
